57cda3cdd7dd4a80a679db433e03d357901ae53e
Curtis Cook <SanzaBlancoAkA2C@gmail.com>
2018-02-03 18:51:58 -0500

    Initial commit

---
+++ app/src/main/java/com/sanzablancoaka2c/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.sanzablancoaka2c.interactivestory.model;
+
+/**
+ * Created by SanzaBlancoAkA2C on 4/15/17.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/com/sanzablancoaka2c/interactivestory/ui/MainActivity.java
@@ -0,0 +1,49 @@
+package com.sanzablancoaka2c.interactivestory.ui;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sanzablancoaka2c.interactivestory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+//                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                startStory(name);
+            }
+        });
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        nameField.setText("");
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
+        startActivity(intent);
+    }
+}
